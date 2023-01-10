c1872df296e40a13dee13c6ccce080cd1bcfafcc
Anderson Sales <andersonsales.mg@gmail.com>
2023-01-10 00:06:52 -0300

    committing code do the main branch

--- GasolineOrEthanol/Calculator/CalculatorVC.swift
+++ GasolineOrEthanol/Calculator/CalculatorVC.swift
@@ -9,32 +9,33 @@ import UIKit
 
 class CalculatorVC: UIViewController {
     
-    // Creating a variable of type CalculatorScreen (it can be used to access the protocol, for example)
+    /// Creating a variable of type CalculatorScreen (it can be used to access the protocol, for example)
     var screen: CalculatorScreen?
     
-    // Creating a variable of type Alert so that this class can show alerts
+    /// Creating a variable of type Alert so that this class can show alerts
     var alert: Alert?
     
     override func loadView() {
-        // Creating an instance of CalculatorScreen and assigning it to the view
+        /// Creating an instance of CalculatorScreen and assigning it to the view
         screen = CalculatorScreen()
         view = screen
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // We need to sign the protocol so that we can use it here in the CalculatorVC class
+        /// We need to sign the protocol so that we can use it here in the CalculatorVC class
         screen?.getAccessToDelegate(delegate: self)
-        // We need to instantiate the alert in the viewDidLoad
+        
+        /// We need to instantiate the alert in the viewDidLoad
         alert = Alert(controller: self)
         
-        // Calling the function to configure textfields
+        /// Calling the function to configure textfields
         configTextFields()
         
-        // Calling the function to hide keyboard (inside the same class)
+        /// Calling the function to hide keyboard (inside the same class)
         configTapGesture()
         
-        // Calling the function to hide keyboard (extension)
+        /// Calling the function to hide keyboard (extension)
         hideKeyboardWhenTappedAround()
     }
     
@@ -43,7 +44,7 @@ class CalculatorVC: UIViewController {
         screen?.ethanolPriceTextField.delegate = self
     }
     
-    // Creating a function to validate whether the textfields have values for the calculation to be performed
+    /// Creating a function to validate whether the textfields have values for the calculation to be performed
     func validateTextFields() -> Bool {
         guard let ethanolPrice = screen?.ethanolPriceTextField.text else { return false }
         guard let gasPrice = screen?.gasPriceTextField.text else { return false }
@@ -66,21 +67,21 @@ extension CalculatorVC: CalculatorScreenProtocol {
         
         if validateTextFields(){
             
-            // Creating the logic to calculate which option is best based on their prices (ethanol or gasoline)
+            /// Creating the logic to calculate which option is best based on their prices (ethanol or gasoline)
             let formatter = NumberFormatter()
             formatter.numberStyle = .decimal
             
             let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPriceTextField.text ?? "0.00") as? Double) ?? 0.00
             let gasPrice: Double = (formatter.number(from: screen?.gasPriceTextField.text ?? "0.00") as? Double) ?? 0.00
             
-            // Created a variable of type ResultVC
+            /// Created a variable of type ResultVC
             var vc: ResultVC?
             
             if ethanolPrice >= gasPrice * 0.7 {
-                //Initializing vc with the best fuel based on calculation
+                ///Initializing vc with the best fuel based on calculation
                 vc = ResultVC(bestFuel: .gas)
             } else {
-                //Initializing vc with the best fuel based on calculation
+                ///Initializing vc with the best fuel based on calculation
                 vc = ResultVC(bestFuel: .ethanol)
             }
             navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
